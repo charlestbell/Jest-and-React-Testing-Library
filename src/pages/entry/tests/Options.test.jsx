c24cb9c2143c887d3d4cfe604a9687ed1e0322fd
C.T <ct.bell@yahoo.com>
2021-10-22 14:32:33 -0400

    Adding testing-library-utils.jsx to add providers to render

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,10 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 
 import Options from "../Options";
-import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
 test("displays image for each scoop option from server", async () => {
-  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
+  render(<Options optionType="scoops" />);
   const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
@@ -13,7 +12,7 @@ test("displays image for each scoop option from server", async () => {
 });
 
 test("displays image for each topping option from server", async () => {
-  render(<Options optionType="toppings" />, { wrapper: OrderDetailsProvider });
+  render(<Options optionType="toppings" />);
   const toppingImages = await screen.findAllByRole("img");
   expect(toppingImages).toHaveLength(3);
 
